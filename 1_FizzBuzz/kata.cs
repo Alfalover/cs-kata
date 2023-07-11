51b3079fa0077c3e2f057e99d8cef29bb3d4cf4b
Antonio Jesús Dávila Molina <AntonioJesus.Davila@vista.com>
2023-07-11 16:11:30 +0200

    jjj

--- 1_FizzBuzz/kata.cs
+++ 1_FizzBuzz/kata.cs
@@ -2,9 +2,49 @@ namespace FizzBuzzKata;
 
 using Xunit;
 using FluentAssertions;
+using System;
 
 public class FizzBuzzShould
 {
+    [Theory]
+    [InlineData(1, "1")]
+    [InlineData(2, "2")]
+    [InlineData(4, "4")]
+    public void ReturnNumberAsStringWhenInputIsNotMultipleOfThreeFiveOrBoth(int number, string expected)
+    {
+        FizzBuzz.compute(number).Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(3)]
+    [InlineData(6)]
+    [InlineData(9)]
+    public void ReturnFizzWhenInputIsMultipleOfThree(int number)
+    {
+        FizzBuzz.compute(number).Should().Be("Fizz");
+    }
+
     [Fact]
-    public void DoSomething() => true.Should().BeTrue();
+    public void ReturnBuzzWhenInputIsFive()
+    {
+        FizzBuzz.compute(5).Should().Be("Buzz");
+    }
+}
+
+internal class FizzBuzz
+{
+    internal static string compute(int number)
+    {
+        if (number % 3 == 0)
+        {
+            return "Fizz";
+        }
+
+        if (number == 5)
+        {
+            return "Buzz";
+        }
+
+        return number.ToString();
+    }
 }
